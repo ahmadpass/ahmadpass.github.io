9e51c769d85d2366fbd4cf0703910596f49df647
ahmadpass <ahmadridwan.andrianto@gmail.com>
2024-10-04 08:41:39 +0700

    add feature "arrow up"

--- js/scripts-ori.js
+++ js/scripts-ori.js
@@ -21,18 +21,22 @@ async function loadTemplateLayout() {
         await $("#navigation").load("shared/navigation-0.html");
         await $("#footer").load("shared/footer.html");
         await $("#postPreview").load("shared/post-preview.html");
+        await $("#arrowup").load("shared/arrow-up.html");
     } else if (index == "1") {
         await $("#navigation").load("../shared/navigation-1.html");
         await $("#footer").load("../shared/footer.html");
         await $("#postPreview").load("../shared/post-preview.html");
+        await $("#arrowup").load("shared/arrow-up.html");
     } else if (index == "2") {
         await $("#navigation").load("../../shared/navigation-2.html");
         await $("#footer").load("../../shared/footer.html");
         await $("#postPreview").load("../../shared/post-preview.html");
+        await $("#arrowup").load("shared/arrow-up.html");
     } else if (index == "3") {
         await $("#navigation").load("../../../shared/navigation-3.html");
         await $("#footer").load("../../../shared/footer.html");
         await $("#postPreview").load("../../../shared/post-preview.html");
+        await $("#arrowup").load("shared/arrow-up.html");
     }
 
 
@@ -83,6 +87,28 @@ async function loadNav() {
 // window.addEventListener('DOMContentLoaded', () => {
 //     loadNav();
 // })
+
+// Function for arrow up
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+// ===========================================================
 
 $(window).on('load', function () {
     setTimeout(() => {
